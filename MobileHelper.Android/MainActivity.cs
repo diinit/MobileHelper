15b9c9228fa6e1729d57c0c0377151c0ca80cf07
Diana <64156992+diinit@users.noreply.github.com>
2021-04-16 22:50:36 +0300

    модуль с картой без API KEY

--- MobileHelper.Android/MainActivity.cs
+++ MobileHelper.Android/MainActivity.cs
@@ -7,13 +7,21 @@ using Android.OS;
 
 namespace MobileHelper.Droid
 {
-    [Activity(Label = "MobileHelper", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "MobileHelper", 
+        Icon = "@mipmap/icon", 
+        Theme = "@style/MainTheme", 
+        MainLauncher = true, 
+        ConfigurationChanges = ConfigChanges.ScreenSize | 
+        ConfigChanges.Orientation | ConfigChanges.UiMode | 
+        ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
+            Xamarin.FormsMaps.Init(this, savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
---
+++ MobileHelper/DealerMap.xaml.cs
@@ -0,0 +1,69 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+using Xamarin.Forms.Xaml;
+
+namespace MobileHelper
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DealerMap : ContentPage
+    {
+        public DealerMap()
+        {
+            InitializeComponent();
+            Task.Delay(2000);
+            UpdateMap();
+        }
+
+        List<DealerPlace> placesList = new List<DealerPlace>();
+
+        private async void UpdateMap()
+        {
+            try
+            {
+                var assembly = IntrospectionExtensions.GetTypeInfo(typeof(DealerMap)).Assembly;
+                Stream stream = assembly.GetManifestResourceStream("MobileHelper.DealerPlaces.json");
+                string text = string.Empty;
+                using (var reader = new StreamReader(stream))
+                {
+                    text = reader.ReadToEnd();
+                }
+
+                var resultObject = JsonConvert.DeserializeObject<DealerPlaces>(text);
+
+                foreach (var place in resultObject.results)
+                {
+                    placesList.Add(new DealerPlace
+                    {
+                        PlaceName = place.name,
+                        Address = place.vicinity,
+                        Location = place.geometry.location,
+                        Position = new Position(place.geometry.location.lat, place.geometry.location.lng),
+                        //Icon = place.icon,
+                        //Distance = $"{GetDistance(lat1, lon1, place.geometry.location.lat, place.geometry.location.lng, DistanceUnit.Kiliometers).ToString("N2")}km",
+                        //OpenNow = GetOpenHours(place?.opening_hours?.open_now)
+                    });
+                }
+
+                MyMap.ItemsSource = placesList;
+                //PlacesListView.ItemsSource = placesList;
+                //var loc = await Xamarin.Essentials.Geolocation.GetLocationAsync();
+                MyMap.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(47.6370891183, -122.123736172), Distance.FromKilometers(100)));
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+        }
+    }
+}
